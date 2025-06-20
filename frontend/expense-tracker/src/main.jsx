d489cc1caf24ed059537474a307b3160bf76b803
Manya Kalra <146841072+manyakalra05@users.noreply.github.com>
2025-06-20 14:55:48 +0530

    Update main.jsx

--- frontend/expense-tracker/src/main.jsx
+++ frontend/expense-tracker/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ import HashRouter
+import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter> {/* ✅ wrap App with HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
-  </StrictMode>,
+  </StrictMode>
 );
