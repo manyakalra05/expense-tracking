352c7c26e2ba1c0ccb62ad605c3809eb3a405024
Manya Kalra <146841072+manyakalra05@users.noreply.github.com>
2025-06-20 11:42:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,3 +52,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+console.log(localStorage.getItem("token")); // should not be null
