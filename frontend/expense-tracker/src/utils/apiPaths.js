eaaffd03a8889fed2ef99a7ccf8724f1e4579668
Manya Kalra <146841072+manyakalra05@users.noreply.github.com>
2026-03-23 02:24:45 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://manyakalra05-financial-tracking.onrender.com";
+export const BASE_URL = "http://localhost:8000";
 // utils/apiPaths.js
 export const API_PATHS = {
   AUTH: {
