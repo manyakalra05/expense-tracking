17a65781f87e6b32b37ab1893ae9808bf39ea920
Manya Kalra <146841072+manyakalra05@users.noreply.github.com>
2026-03-23 03:11:02 +0530

    Update Last30DaysExpenses.jsx

--- frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
+++ frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -14,7 +14,7 @@ const Last30DaysExpenses = ({ data }) => {
   return (
     <div className="card col-span-1">
       <div className="flex items-center justify-between">
-        <h5 className="text-lg">Last 30 Days Expenses</h5>
+        <h5 className="text-lg">Last 69 Days Expenses</h5>
       </div>
 
       <CustomBarChart data = {chartData}/>
